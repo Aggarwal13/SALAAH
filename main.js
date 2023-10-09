83483b062db62b316e23c4c09e5ce28e6890b5de
Aggarwal13 <113710142+Aggarwal13@users.noreply.github.com>
2023-10-09 18:48:00 +0530

    Add files via upload
    done

---
+++ main.js
@@ -0,0 +1,10 @@
+
+    
+ const d=document.querySelector('#d')
+ d.addEventListener('click',r4Function)
+  function r4Function(){
+      document.body.style.backgroundColor="black";
+      
+    
+
+  }
